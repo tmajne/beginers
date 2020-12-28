f2ebff234429d25e1fe184113d604f5c27a1cd2f
tomasz.tomczyk <tomasz.tomczyk@displate.com>
2020-12-28 22:53:47 +0100

    one

--- scripts/one/example1.php
+++ scripts/one/example1.php
@@ -21,19 +21,14 @@
  * Interfejs to tak naprawdę publiczny kontrakt, który jest nakładany na klasę go implementującą.
  * Klasa implementująca musi spełnić ten kontrakt, czyli zaimplementować metody z interfejsu
  *
- * W uproszczeniu możemy powiedzieć, że interfejsów używamy gdy chcemy narzucić programistom (również sobie)
- * zbiór metod, które powinny być zaimplementowane w danej klasie.
+ * W uproszczeniu możemy powiedzieć, że interfejsów używamy gdy chcemy narzucić programistom (również sobie) zbiór metod, które powinny być zaimplementowane w danej klasie.
  *
- * Pierwszą myślą która nam przychodzi kiedy warto by było użyć interfejsu, to dziedziczenie w przypadku,
- * gdy chcemy wymusić aby każde dziecko miało pewien określony zbiór metod.
+ * Pierwszą myślą która nam przychodzi kiedy warto by było użyć interfejsu, to dziedziczenie w przypadku, gdy chcemy wymusić aby każde dziecko miało pewien określony zbiór metod.
  *
  * Dla osób trochę bardziej zaawansowanych.
  *
- * Drugą trochę "nie oczywistą" dla osób bardziej początkujących jest sytuacja gdy tworzymy jakiś serwis/bibliotekę
- * i dopuszczamy możliwość stworzenie jej zupełnie nowej implementacji, na przykład w innej technologi.
- * Dobrym przykładem jest np serwis do zapisu danych, możemy dane zapisywać w pliku, bazie danych, pamięci w jakimś
- * zewnętrznym serwisie z którym komunikujemy się na przykład przez http. Każdy taki serwis musi spełniać ten sam
- * "kontrakt" jednak nie muszą one dziedziczyć po wspólnym rodzicu (pamiętamy, kompozycja ponad dziedziczenie)
+ * Drugą trochę "nie oczywistą" dla osób bardziej początkujących jest sytuacja gdy tworzymy jakiś serwis/bibliotekę i dopuszczamy możliwość stworzenie jej zupełnie nowej implementacji, na przykład w innej technologi.
+ * Dobrym przykładem jest np serwis do zapisu danych, możemy dane zapisywać w pliku, bazie danych, pamięci w jakimś zewnętrznym serwisie z którym komunikujemy się na przykład przez http. Każdy taki serwis musi spełniać ten sam "kontrakt" jednak nie muszą one dziedziczyć po wspólnym rodzicu (pamiętamy, kompozycja ponad dziedziczenie)
  * Np. mamy interfejs:
  * Storage
  * i jego implementacje
@@ -48,13 +43,10 @@
  *
  *
  * Klasy abstrakcyjne wychodzą poza interfejs ponieważ dopuszczają implementację samej metody czyli jej definicję.
- * Tak więc jeśli mamy jakiś wspólny zbiór metod/właściwości dla wszystkich dzieci to możemy je wyciągnąć do klasy
- * abstrakcyjnej, a metody które wiemy, że ich definicja czyli ciało metody będzie się różnić w zależności od rodzaju
- * dziecka pozostawiamy do implementacji już w samych dzieciach, czyli klasach dziedziczących po klasie abstrakcyjnej
+ * Tak więc jeśli mamy jakiś wspólny zbiór metod/właściwości dla wszystkich dzieci to możemy je wyciągnąć do klasy abstrakcyjnej, a metody które wiemy, że ich definicja czyli ciało metody będzie się różnić w zależności od rodzaju dziecka pozostawiamy do implementacji już w samych dzieciach, czyli klasach dziedziczących po klasie abstrakcyjnej
  *
  * Dobrym pomysłem jest aby klasa abstrakcyjna implementowała interfejs (czyli "kontrakt", który musi spełnić).
- * W samej klasie jak już zostało wspomniane implementujemy część wspólną kontraktu dla wszystkich dzieci, a metody,
- * których implementacja będzie się różnić w zależności od dziecka pozostawiamy już do implementacji w dzieciach
+ * W samej klasie jak już zostało wspomniane implementujemy część wspólną kontraktu dla wszystkich dzieci, a metody, których implementacja będzie się różnić w zależności od dziecka pozostawiamy już do implementacji w dzieciach
  *
  * Kiedy używać klasy abstrakcyjnej (patrz wyżej)
  */
